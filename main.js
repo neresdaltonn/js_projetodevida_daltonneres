cf0a7c755f30efad421fe9c22e85d46f08493d7a
Dalton José Neres <145723466+neresdaltonn@users.noreply.github.com>
2025-04-29 07:56:55 -0300

    29/04

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-06-05T14:00:00");
 const tempoObjetivo2 = new Date("2025-06-26T15:00:00");
 const tempoObjetivo3 = new Date("2025-12-20T20:00:00");
-const tempoObjetivo4 = new Date("2025-11-30T14:30:00");
+const tempoObjetivo4 = new Date("2025-11-30T14:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
