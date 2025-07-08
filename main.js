fb19f71b51827814dd947767637ac048e3a5a149
Dalton José Neres <145723466+neresdaltonn@users.noreply.github.com>
2025-07-08 07:45:27 -0300

    08/07

--- main.js
+++ main.js
@@ -37,10 +37,7 @@ function calculaTempo(tempoObjetivo) {
 
     if(tempoFinal>0){
         return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
-    }else{
-        return "Prazo Finalizado!"
     }
-}
 
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
